e0ecd55d47dab2c303e6e833442bbf3c71639903
kd <0xbeefdead@gmail.com>
2020-07-25 19:56:58 +0530

    merging vertical boxes based upon font similarity

--- notebooks/box_font_evalutions.py
+++ notebooks/box_font_evalutions.py
@@ -1,7 +1,8 @@
 import pandas as pd
 
-def are_hlines(df, configs, idx1, idx2, debug=False):
-    space = abs(df.iloc[idx1]['text_top'] - df.iloc[idx2]['text_top'])
-    if debug:
-        print('are_hlines:: idx1: %d, idx2: %d, space: %d' % (idx1, idx2, space))
-    return space <= configs['SUPERSCRIPT_HEIGHT_DIFFERENCE']
+def are_lines_fonts_similar(df, configs, idx1, idx2, debug=False):
+    if (abs(df.iloc[idx1]['font_size'] - df.iloc[idx2]['font_size']) < 2.0) \
+            and (df.iloc[idx1]['font_family'] == df.iloc[idx2]['font_family']):
+        return True
+    return False
+    
--- notebooks/box_vertical_evalutions.py
+++ notebooks/box_vertical_evalutions.py
@@ -1,7 +1,8 @@
 import pandas as pd
 
 def are_vlines(df, configs, idx1, idx2, debug=False):
-    space = abs(df.iloc[idx1]['text_top'] - df.iloc[idx2]['text_top'])
-    if debug:
-        print('are_hlines:: idx1: %d, idx2: %d, space: %d' % (idx1, idx2, space))
-    return space <= configs['SUPERSCRIPT_HEIGHT_DIFFERENCE']
+    first_idx, sec_idx  = get_lines_upper_lower(df, idx1, idx2)
+    space               = df.iloc[sec_idx]['text_top'] - (df.iloc[first_idx]['text_top'] + df.iloc[first_idx]['text_height'])
+    if space > configs['VERTICAL_SPACE_TOO_CLOSE']:
+        return True
+    return False
