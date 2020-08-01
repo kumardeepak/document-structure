ec890ba788daa883f97b8d2688cd1c5b500052d1
kd <0xbeefdead@gmail.com>
2020-08-01 17:31:13 +0530

    introduced normalization function to correct left & width

--- notebooks/xml_document_info.py
+++ notebooks/xml_document_info.py
@@ -9,6 +9,21 @@ from utilities import (extract_image_from_pdf, extract_xml_from_digital_pdf,
 def get_document_width_height(pages):
     return int(pages[0].attrib['width']), int(pages[0].attrib['height'])
 
+'''
+    -    Normalizes the left & width value currently
+'''
+def normalize_page_xml_df(in_df, width, height):
+    df = in_df.copy(deep=True)
+    
+    for index, row in df.iterrows():
+        if row['text_left'] < 0:
+            df.at[index, 'text_left'] = abs(in_df.iloc[index]['text_left'])
+            
+        if row['text_left'] + row['text_width'] > width:
+            df.at[index, 'text_width'] = in_df.iloc[index-1]['text_width']
+    
+    return df
+
 def get_xml_info(filepath):
     xml             = get_xmltree(filepath)
     tag             = 'page'
@@ -33,29 +48,12 @@ def get_xml_info(filepath):
         
         for index, text in enumerate(texts):
             p_t, p_l, p_w, p_h, t_t, t_l, t_w, t_h, f_size, f_family, f_color, t = get_page_text_element_attrib(fonts, page, text)
-            right_empty_space = 4 * (len(t) - len(t.rstrip()) + 1)
-            left_empty_space  = 4 * (len(t) - len(t.lstrip()))
-
             if len(t.strip()) < 1:
                 continue
 
             t_ts.append(t_t)
-            '''
-                outlier condition. text_left has been reported to negative.
-                                        - currently we have made it +ve value.
-                                    text_width sometimes crosses page width. In order to handle it
-                                        - count empty space and multiply by factor 4. Ideally it is dependent upon font size.
-                                        - also ignoring left_empty_space
-            '''
-            
-            t_ls.append(abs(t_l))
-
-            if (t_w + t_l) > width:
-                t_w1 = (t_w - right_empty_space)
-                t_ws.append(t_w1)
-            else:
-                t_ws.append(t_w)
-
+            t_ls.append(t_l)
+            t_ws.append(t_w)
             t_hs.append(t_h)
             f_sizes.append(f_size)
             f_familys.append(f_family)
@@ -71,6 +69,6 @@ def get_xml_info(filepath):
         df.rename(columns={'index':'xml_index'},inplace=True)
         #print(df.head())
     
-        dfs.append(df)
+        dfs.append(normalize_page_xml_df(df, width, height))
 
     return dfs, width, height
